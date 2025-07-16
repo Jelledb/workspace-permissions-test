318912b7b6e0fd9e45498958d47843fe98ca7a45
Jelle den Burger <Jelledb@users.noreply.github.com>
2025-07-16 09:30:45 +0000

    init

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+output "wp" {
+  description = "A list of objects with 6 properties"
+  value = var.wp
+}
---
+++ vars.tf
@@ -0,0 +1,12 @@
+variable "wp" {
+  description = "A list of objects with 6 properties"
+  type = list(object({
+    meshIdentifier = string
+    username       = string
+    firstName      = string
+    lastName       = string
+    email          = string
+    euid           = string
+    roles          = list(string)
+  }))
+}
